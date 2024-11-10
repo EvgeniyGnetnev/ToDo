799415521f3397474748a16e7075dc3fc52cd8b5
EvgeniyGnetnev <e.gnetnev@yandex.ru>
2024-11-10 12:54:37 +0300

    Импортировал gin и добавил handler

--- cmd/go.mod
+++
@@ -1,5 +0,0 @@
-module main
-
-go 1.23.2
-
-require github.com/EvgeniyGnetnev/ToDo v0.0.0-20241109095726-7d9f8f4783b7
--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"log"
 
-	todo "github.com/EvgeniyGnetnev/ToDo"
+	todo "todo"
+	handler "todo/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler{})
 	srv := new(todo.Server)
 	if err := srv.Run("8000"); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
