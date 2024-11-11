7afa476c9cf8853ab633a71684828afbbcc3fcc3
EvgeniyGnetnev <chaklittle@yandex.ru>
2024-11-11 18:20:31 +0300

    добавил подключение к БД из приложения

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"log"
+	"os"
+
 	todo "github.com/EvgeniyGnetnev/ToDo"
 	"github.com/EvgeniyGnetnev/ToDo/pkg/handler"
 	"github.com/EvgeniyGnetnev/ToDo/pkg/repository"
 	"github.com/EvgeniyGnetnev/ToDo/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 )
 
@@ -13,7 +17,23 @@ func main() {
 	if err := initConfig(); err != nil {
 		log.Fatalf("error initializing configs: %s", err.Error())
 	}
-	repos := repository.NewRepository()
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env cariables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
