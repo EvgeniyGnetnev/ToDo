d20f2329d91fe0951384d625aeabb0ef0a386aca
EvgeniyGnetnev <chaklittle@yandex.ru>
2024-11-14 20:57:16 +0300

    Добавил создание элемента и получение всех элементов списка

--- todo.go
+++ todo.go
@@ -14,11 +14,11 @@ type UserList struct {
 	ListId int
 }
 
-type TodoItems struct {
-	Id          int    `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Done        bool   `json:"done"`
+type TodoItem struct {
+	Id          int    `json:"id" db:"id"`
+	Title       string `json:"title" db:"title" binding:"required"`
+	Description string `json:"description" db:"description"`
+	Done        bool   `json:"done" db:"done"`
 }
 
 type ListsItem struct {
